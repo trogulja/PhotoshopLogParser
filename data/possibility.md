39558e09253bdaa44656e7b34ea073fa02231fed
trogulja <tibor.rogulja@tiskarazagreb.hr>
2020-10-05 08:45:33 +0200

    testing

--- data/possibility.md
+++ data/possibility.md
@@ -1,36 +1,32 @@
 # rules
 
-- [ ] file open
-  - [ ] create new object
-- [ ] action count
+- [x] file open
+  - [x] create new object
+  - [ ] if claro original file (1234original.???), ignore?
+- [x] action count
   - [ ] assign max time for action type
-- [ ] file save or file close
-  - [ ] if match in db, add to file actions count
-  - [ ] reset actions counter to 0
-  - [ ] find all open files, set start time to this
-- [ ] file save
-  - [ ] update save time
-  - [ ] update path
-- [ ] file close
-  - [ ] update close time
+- [x] file save or file close
+  - [x] if match in db, add to file actions count
+  - [x] reset actions counter to 0
+  - [x] find all open files, set start time to this
+- [x] file save
+  - [x] ignore if we're working on .psb file (smart object) - 
+  - [x] update save time
+  - [x] update path
+- [x] file close
+  - [x] update close time
 
-
-- [ ] any action bound to first file saved or closed
-  - [ ] count them
-  - [ ] if that file has open event (.psb don't have)
-- [ ] for any open file, reset open time to match last close or save event
-  - [ ] if that file has open event (.psb!)
 
 # times
 
-- [ ] file open
-- [ ] file save
-- [ ] file close
-- [ ] file start = open
-  - [ ] if multiple images open, set start to last image closed - until all images are closed
-- [ ] file end = close
+- [x] file open
+- [x] file save
+- [x] file close
+- [x] file start = open
+  - [x] if multiple images open, set start to last image closed - until all images are closed
+- [x] file end = close
   - [ ] if there is something in between save and close events, file end = save + 2sec
-- [ ] duration = end - start (in seconds?)
+- [x] duration = end - start (in seconds?)
 - [ ] active = Math.min(duration, sum action count max time)
 
 # edge cases
--- src/lib/parsePhotoshopLogFile.js
+++ src/lib/parsePhotoshopLogFile.js
@@ -4,6 +4,7 @@ const readline = require('readline');
 const Datastore = require('nedb-promises');
 const { env } = require('process');
 const db = new Datastore();
+db.ensureIndex({ fieldName: 'name' });
 
 async function parsePhotoshopLogFile(file) {
   // file.path
@@ -21,49 +22,30 @@ async function parsePhotoshopLogFile(file) {
 
   let i = 0;
   let action = 0;
+  // let opencount = 0;
   for await (let line of rl) {
     let event = 'ignore';
     if (psRegex.action.test(line)) {
       action += 1;
-      // event = 'action';
-      // await handleAction(event, line, i);
     } else {
       if (psRegex.open.test(line)) event = 'open';
       else if (psRegex.save.test(line)) event = 'save';
       else if (psRegex.close.test(line)) event = 'close';
 
-      await handleFile(event, line, i);
+      await handleEvent(event, line, i);
     }
 
     i += 1;
   }
 
-  async function handleAction(event, line, i) {
-    if (event === 'action') {
-      action += 1;
-    } else {
-      if (action > 0) {
-        console.log(`Line: ${i}, Action is: ${action}`);
-        const match = await db.findOne({ actions: 0 }).sort({ i: -1 });
-        if (!match) console.log(line);
-        console.log(match);
-        const res = await db.update(
-          { _id: match._id },
-          { $set: { actions: action } }
-        );
-        console.log(res);
-        action = 0;
-      }
-    }
-    return true;
-  }
-
-  async function handleFile(event, line, i) {
+  async function handleEvent(event, line, i) {
     const m = psRegex[event].exec(line);
     const date = new Date(m[1], m[2] - 1, m[3], m[4], m[5], m[6]);
     const name = event === 'save' ? path.basename(m[7]) : m[7];
 
     if (event === 'open') {
+      // TODO - what if we're opening a claro comparison file?
+      // opencount = opencount + 1;
       await db.insert({
         i,
         name,
@@ -71,22 +53,35 @@ async function parsePhotoshopLogFile(file) {
         open: date,
         save: null,
         close: null,
+        start: date,
+        end: null,
         actions: 0,
         duration: 0,
       });
     } else {
       const match = await db.findOne({ name }).sort({ i: -1 });
-      // if (!match) console.log(`We couldn't find a match for event ${event} for name ${name}`);
+      if (!match) console.log(`We couldn't find a match for event ${event} for name ${name}`);
       if (!match) return false;
 
+      // Update total actions for this file
+      let totalActions = match.actions + action;
+      await db.update({ _id: match._id }, { $set: { actions: totalActions } });
+      action = 0;
+
+      // Update all of the times we need updating
       let update = {};
       if (event === 'save') {
-        update = { save: date, path: m[7] };
+        update = { save: date, end: date, path: m[7], duration: calcDuration(match.start, date) };
       } else if (event === 'close') {
-        update = { close: date, duration: calcDuration(match.open, date) };
+        update = { close: date, end: date, duration: calcDuration(match.start, date) };
       }
-
       await db.update({ _id: match._id }, { $set: update });
+
+      // Find all open files and set start time to this
+      const multimatch = await db.find({ save: null, close: null });
+      for (const doc of multimatch) {
+        await db.update({ _id: doc._id }, { $set: { start: date } });
+      }
     }
 
     return true;
@@ -98,7 +93,7 @@ async function parsePhotoshopLogFile(file) {
 
   // const results = await db.find({});
   // console.log(results)
-  return file.path;
+  return await db.find({}).sort({ i: 1 });
 }
 
 module.exports = parsePhotoshopLogFile;
